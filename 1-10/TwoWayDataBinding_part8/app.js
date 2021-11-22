1d9c8f773be28f5a89b064a31806eddff0b87861
furiousnur <furiousnur@gmail.com>
2021-11-22 17:11:10 +0600

    two way data binding

---
+++ 1-10/TwoWayDataBinding_part8/app.js
@@ -0,0 +1,25 @@
+var app = Vue.createApp({
+     data(){
+         return{
+            num:"",
+            result:"",
+         }
+     },
+    methods: {
+        getDouble(){
+            this.result = this.num * 2;
+        },
+        getSquare(){
+            this.result = this.num * this.num;
+        },
+        addTen(){
+            this.result = this.num + 10;
+        },
+        reset(){
+            this.num = null;
+            this.result = null;
+        }
+    }
+});
+
+app.mount("#app");
