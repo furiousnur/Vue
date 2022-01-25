78b8571dfc1ed10ce61f623f805f7bafdbfc7dc7
furiousnur <furiousnur@gmail.com>
2022-01-25 16:57:39 +0600

    get box coord using event

---
+++ Vue Project/event/app.js
@@ -0,0 +1,17 @@
+new Vue({
+   el: "#app",
+   data: {
+      name: "Nur Alam",
+       x: 0,
+       y: 0,
+   },
+    methods: {
+        changeName(){
+            this.name = 'Nur'
+        },
+        getCoord(){
+            this.x = event.clientX;
+            this.y = event.clientY;
+        }
+    }
+});
