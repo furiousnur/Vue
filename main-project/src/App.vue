21856c46e64a962b505991392bde4c3db2c96fd7
furiousnur <furiousnur@gmail.com>
2022-01-26 15:33:24 +0600

    create a component & locally import

--- main-project/src/App.vue
+++ main-project/src/App.vue
@@ -1,18 +1,26 @@
  <template>
       <div class="div">
+        <Header/>
+        <Footer/>
         <p>{{name}}</p>
         <p>{{age}}</p>
       </div>
  </template>
 
  <script>
+      import Header from './Components/layouts/Header';
+      import Footer from './Components/layouts/Footer';
       export default {
           data(){
               return{
                   name: 'Nur',
                   age: 26
               }
-          }
+          },
+        components: {
+            Header,
+            Footer
+        }
       }
  </script>
 
